1a8756b8a27414b3230b2ba821d4610058066f9a
JIANHONG OU <jianhong@users.noreply.github.com>
2021-09-17 13:09:09 -0400

    fix the bug for MAPS_peak_formatting.r which can not handle empty data.

--- bin/MAPS_peak_formatting.r
+++ bin/MAPS_peak_formatting.r
@@ -13,7 +13,7 @@ for(inf in infs){
 
     peaks = as.data.table(subset(peaks_raw, ClusterType != 'Singleton' | (ClusterType == 'Singleton' & fdr < 1e-4))) # remove singletons
     if(nrow(peaks)==0){
-        peaks_final <- peaks
+        peaks$bin1_end <- peaks$bin2_end <- peaks$summit <- numeric(0)
     }else{
         peaks[, summit := 1*(fdr == min(fdr)), by = lab]
         peaks$summit[ peaks$ClusterType == 'Singleton'] = 1
@@ -23,9 +23,9 @@ for(inf in infs){
 
         peaks$bin1_end = peaks$bin1_mid + RESOLUTION
         peaks$bin2_end = peaks$bin2_mid + RESOLUTION
-        peaks_final = subset(peaks, select = c("chr", "bin1_mid", "bin1_end", "chr", "bin2_mid", "bin2_end", "count", "expected2", "fdr", "lab", "ClusterSize", "ClusterType", "NegLog10P", "summit"))
-        colnames(peaks_final) = c('chr1', 'start1', 'end1', 'chr2', 'start2', 'end2', 'count', 'expected', 'fdr', 'ClusterLabel', 'ClusterSize', 'ClusterType', 'ClusterNegLog10P', 'ClusterSummit')
     }
+    peaks_final = subset(peaks, select = c("chr", "bin1_mid", "bin1_end", "chr", "bin2_mid", "bin2_end", "count", "expected2", "fdr", "lab", "ClusterSize", "ClusterType", "NegLog10P", "summit"))
+    colnames(peaks_final) = c('chr1', 'start1', 'end1', 'chr2', 'start2', 'end2', 'count', 'expected', 'fdr', 'ClusterLabel', 'ClusterSize', 'ClusterType', 'ClusterNegLog10P', 'ClusterSummit')
     fout = sub(".peaks",'.sig3Dinteractions.bedpe',inf)
     write.table(peaks_final, fout, row.names = FALSE, col.names = TRUE, quote=FALSE, sep='\t')
 }
