67eb842d6ee4a8f1dcb11e977b035e0ca4bff9da
JIANHONG OU <jianhong@users.noreply.github.com>
2023-07-13 11:57:53 -0400

    move trimbedgraph source to bin folder.

---
+++ bin/trimbedgraph.r
@@ -0,0 +1,77 @@
+#!/usr/bin/env Rscript
+#######################################################################
+#######################################################################
+## Trim bedgraph file by genome size
+## Copyright (c) 2021 Jianhong Ou (jianhong.ou@gmail.com)
+## This source code is licensed under the MIT license
+## arg is the format of input files
+#######################################################################
+#######################################################################
+args = commandArgs(TRUE)
+parse_args <- function(options, args){
+    lapply(options, function(.ele){
+        n <- .ele[c(1, 2)]
+        if(any(n %in% args)){
+            if(.ele[3]=="logical"){
+                list(arg=TRUE, ids=which(args %in% n))
+            }else{
+                id <- which(args %in% n)[1]
+                x <- args[id+1]
+                mode(x) <- .ele[3]
+                list(arg=x, ids=c(id, id+1))
+            }
+        }else{
+            NULL
+        }
+    })
+}
+option_list <- list("task_process"=c("--task_process", "-t", "character"),
+                    "format"=c("--format", "-f", "character"),
+                    "chrom_size"=c("--chrom_size", "-l", "character"))
+opt <- parse_args(option_list, args)
+to_be_removed <- unique(unlist(lapply(opt, function(.ele) .ele$ids)))
+stopifnot(length(opt$format)>0)
+format <- opt$format$arg
+stopifnot(length(opt$chrom_size)>0)
+genome <- read.delim(opt$chrom_size$arg, header=FALSE)
+genome <- with(genome, GRanges(V1, IRanges(1, V2)))
+fn <- args[-to_be_removed]
+sl <- width(genome)
+names(sl) <- as.character(seqnames(genome))
+stopifnot(length(opt$task_process)>0)
+pkgs <- c("rtracklayer")
+versions <- paste0(opt$task_process$arg, ":")
+for(pkg in pkgs){
+    # load library
+    library(pkg, character.only=TRUE)
+    # parepare for versions.yml
+    versions <- c(versions,
+                  paste0("    ", pkg, ": ", as.character(packageVersion(pkg))))
+}
+writeLines(versions, "versions.yml") # write versions.yml
+
+# trim bed file
+for(f in fn){
+    d <- import(f, format=format, which=genome)
+    if(tolower(format)=="wig"){
+        d <- split(d, seqnames(d))
+        d <- lapply(d, function(.d){
+            w <- diff(end(.d))
+            if(length(w)){
+                w <- c(w, max(w, na.rm=TRUE))
+                width(.d) <- w
+            }
+            .d
+        })
+        d <- unlist(GRangesList(d))
+    }
+    if(length(mcols(d)[, "score"])){
+        d <- d[!is.na(mcols(d)[, "score"])]
+    }
+    seqlengths(d) <- sl[seqlevels(d)]
+    d <- trim(d)
+    seqlevels(d) <- sort(seqlevels(d), method='radix')
+    d <- sort(d)
+    n <- sub("\\..*?$", ".trimmed.bedgraph", f)
+    export(d, n, format='bedGraph')
+}
--- modules/local/bioc/trimbedgraph.nf
+++ modules/local/bioc/trimbedgraph.nf
@@ -19,55 +19,11 @@ process BEDGRAPH_TRIM {
     script:
     def args = task.ext.args ?: 'bedGraph'
     """
-    #!/usr/bin/env Rscript
-    #######################################################################
-    #######################################################################
-    ## Trim bedgraph file by genome size
-    ## Copyright (c) 2021 Jianhong Ou (jianhong.ou@gmail.com)
-    ## This source code is licensed under the MIT license
-    ## arg is the format of input files
-    #######################################################################
-    #######################################################################
-    pkgs <- c("rtracklayer")
-    versions <- c("${task.process}:")
-    for(pkg in pkgs){
-        # load library
-        library(pkg, character.only=TRUE)
-        # parepare for versions.yml
-        versions <- c(versions,
-            paste0("    ", pkg, ": ", as.character(packageVersion(pkg))))
-    }
-    writeLines(versions, "versions.yml") # write versions.yml
+    trimbedgraph.r \\
+        --format $args \\
+        --chrom_size $sizes \\
+        $bedgraph
 
-    format="$args"
-    fn <- strsplit("$bedgraph", " ")[[1]]
-    genome <- read.delim("$sizes", header=FALSE)
-    genome <- with(genome, GRanges(V1, IRanges(1, V2)))
-    sl <- width(genome)
-    names(sl) <- as.character(seqnames(genome))
-    for(f in fn){
-        d <- import(f, format=format, which=genome)
-        if(tolower(format)=="wig"){
-            d <- split(d, seqnames(d))
-            d <- lapply(d, function(.d){
-                w <- diff(end(.d))
-                if(length(w)){
-                    w <- c(w, max(w, na.rm=TRUE))
-                    width(.d) <- w
-                }
-                .d
-            })
-            d <- unlist(GRangesList(d))
-        }
-        if(length(mcols(d)[, "score"])){
-            d <- d[!is.na(mcols(d)[, "score"])]
-        }
-        seqlengths(d) <- sl[seqlevels(d)]
-        d <- trim(d)
-        seqlevels(d) <- sort(seqlevels(d), method='radix')
-        d <- sort(d)
-        n <- sub("\\\\..*?\$", ".trimmed.bedgraph", f)
-        export(d, n, format='bedGraph')
-    }
+    # *.version.txt files will be created in the rscripts
     """
 }
